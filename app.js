262c299a7fe2c40c08661ab736ea9501f3e275ad
Moutinho <alexandre.moutinho@live.fr>
2023-01-28 16:49:21 +0100

    :wrench: Linter configuration #6

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // ESM
-import Fastify from "fastify";
+import Fastify from 'fastify'
 const fastify = Fastify({
-  logger: true,
-});
+  logger: true
+})
 
-
-fastify.get("/", async (request, reply) => {
-  reply.type("application/json").code(200);
-  return { hello: "world" };
-});
+fastify.get('/', async (request, reply) => {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+})
 
 fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) throw err;
+  if (err) throw err
   // Server is now listening on ${address}
-});
+})
